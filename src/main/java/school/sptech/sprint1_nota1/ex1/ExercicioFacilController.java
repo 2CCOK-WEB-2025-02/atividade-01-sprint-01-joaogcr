3331dcf6f834bada274e914d77590aca886e3a7e
joaogcr <joao.nribeiro@sptech.school>
2025-08-19 19:54:58 -0300

    Atividade finalizada

--- src/main/java/school/sptech/sprint1_nota1/ex1/ExercicioFacilController.java
+++ src/main/java/school/sptech/sprint1_nota1/ex1/ExercicioFacilController.java
@@ -1,13 +1,28 @@
 package school.sptech.sprint1_nota1.ex1;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExercicioFacilController {
 
     @GetMapping("/ex-01/{palavra}")
-    public Boolean exercicioFacil(String palavra) {
-        return null;
+    public Boolean exercicioFacil(@PathVariable String palavra) {
+
+        if (palavra == null) return false;
+
+        String minuscula = palavra.toLowerCase();
+        int i = 0, j = minuscula.length() - 1;
+
+        while (i < j) {
+            if (minuscula.charAt(i) != minuscula.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
     }
+
 }
--- src/main/java/school/sptech/sprint1_nota1/ex3/ExercicioDificilController.java
+++ src/main/java/school/sptech/sprint1_nota1/ex3/ExercicioDificilController.java
@@ -8,7 +8,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class ExercicioDificilController {
 
     @GetMapping("/ex-03/{n}")
-    public ExercicioDificilResponse exercicioDificil(int n) {
-        return null;
+    public ExercicioDificilResponse exercicioDificil(@PathVariable int n) {
+        if (n <= 0) {
+            return new ExercicioDificilResponse(0, 0);
+        }
+
+        if (n == 1) {
+            return new ExercicioDificilResponse(1, 1);
+        }
+
+        int a = 0, b = 1, enesimo = 1, soma = 1;
+
+        for (int i = 2; i <= n; i++) {
+            enesimo = a + b;
+            a = b;
+            b = enesimo;
+            soma += enesimo;
+        }
+
+        return new ExercicioDificilResponse(enesimo, soma);
     }
 }
